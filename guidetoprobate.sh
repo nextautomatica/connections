f960934e52db523d7ba072f0af0d5a7201577cfb
NextAutomatica <134034444+nextautomatica@users.noreply.github.com>
2024-01-08 16:47:41 -0500

    Update guidetoprobate.sh

--- guidetoprobate.sh
+++ guidetoprobate.sh
@@ -1,4 +1,4 @@
-curl https://nxa.up.railway.app/api/v1/prediction/8d360ecd-3b43-4caf-8b2e-e2a8f9878dba \
+curl https://nxa.up.railway.app/api/v1/prediction/[seed] \
      -X POST \
      -d '{"question": "[prompt]"}' \
      -H "Content-Type: application/json"
