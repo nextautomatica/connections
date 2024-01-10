feb54793f1ff00eae36700d3d8d1f25b021a4bd6
NextAutomatica <134034444+nextautomatica@users.noreply.github.com>
2024-01-10 12:03:16 -0500

    Create sarah.sh

---
+++ sarah.sh
@@ -0,0 +1,4 @@
+curl https://nxa.up.railway.app/api/v1/prediction/4aef4116-2913-4c91-8f42-288a253ad06d \
+     -X POST \
+     -d '{"question": "[prompt]"}' \
+     -H "Content-Type: application/json"
